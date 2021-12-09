21fa94d6ce1ef8f7ec27b2a09459ab2732e75d26
Cervac Petru <petru.cervac@gmail.com>
2021-12-09 20:13:24 +0200

    Started implementing the Left navigation panel

---
+++ MockViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace AdrianRobot;
+
+public class MockViewModel
+{
+    public static MockViewModel Instance { get; } = new MockViewModel();
+
+    public  ObservableCollection<int> Data { get; } = new ObservableCollection<int>() { 1, 2, 3 };
+}
