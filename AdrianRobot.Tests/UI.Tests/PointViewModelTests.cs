6c565d3f70eaf0066f1644a1406640d0ec1afb99
Cervac Petru <petru.cervac@gmail.com>
2021-12-12 18:52:33 +0200

    Added the Name property to PointViewModel

---
+++ AdrianRobot.Tests/UI.Tests/PointViewModelTests.cs
@@ -0,0 +1,14 @@
+﻿namespace AdrianRobot.Tests;
+
+public class PointViewModelTests
+{
+    [Fact]
+    public void PointNameIsTheRightOne()
+    {
+        var point = new Point(new(), "Point", 100, 150);
+
+        var sut = new PointViewModel(point);
+
+        sut.Name.Should().Be($"Point (y: 100, z: 150)");
+    }
+}
--- AdrianRobot/UI/PointViewModel.cs
+++ AdrianRobot/UI/PointViewModel.cs
@@ -5,4 +5,5 @@ public class PointViewModel
     public PointViewModel(Point point) => Point = point ?? throw new System.ArgumentNullException(nameof(point));
 
     public Point Point { get; }
+    public string Name => $"{Point.Name} (y: {Point.MotorYPosition}, z: {Point.MotorZPosition})";
 }
